68112eedb3d900772de00369be612103bc6d14d1
Narimori <ocelot_warhead_9p@icloud.com>
2025-06-26 03:48:35 +0900

    Initial commit

---
+++ src/main/java/org/narimori/fable/FableMod.java
@@ -0,0 +1,9 @@
+package org.narimori.fable;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class FableMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
